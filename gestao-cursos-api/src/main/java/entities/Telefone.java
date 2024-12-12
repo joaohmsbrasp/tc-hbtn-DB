3c4b791b0e1e704cf382c86c41ba51ec8c251e99
Ever Santoro - LYNCAS <ever.santoro.ext@fibrasil.com.br>
2024-12-12 09:41:13 -0300

    2824

---
+++ gestao-cursos-api/src/main/java/entities/Telefone.java
@@ -0,0 +1,52 @@
+package entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Telefone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String DDD;
+    private String numero;
+
+    @ManyToOne
+    @JoinColumn(name = "aluno_id")
+    private Aluno aluno;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDDD() {
+		return DDD;
+	}
+
+	public void setDDD(String dDD) {
+		DDD = dDD;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public Aluno getAluno() {
+		return aluno;
+	}
+
+	public void setAluno(Aluno aluno) {
+		this.aluno = aluno;
+	}
+
+    
+}
---
+++ gestao-cursos-api/src/main/java/models/CursoModel.java
@@ -0,0 +1,84 @@
+package models;
+
+import entities.Curso;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.util.List;
+
+public class CursoModel {
+
+    private EntityManagerFactory emf;
+
+    public CursoModel() {
+        this.emf = Persistence.createEntityManagerFactory("gestao-cursos-jpa");
+    }
+
+    public void create(Curso curso) {
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+            em.persist(curso);
+            em.getTransaction().commit();
+            System.out.println("Curso criado com sucesso!");
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            System.err.println("Erro ao criar o curso: " + e.getMessage());
+        } finally {
+            em.close();
+        }
+    }
+
+    public Curso findById(Long id) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            return em.find(Curso.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<Curso> findAll() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            return em.createQuery("SELECT c FROM Curso c", Curso.class).getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public void update(Curso curso) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.merge(curso);
+            em.getTransaction().commit();
+            System.out.println("Curso atualizado com sucesso!");
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            System.err.println("Erro ao atualizar o curso: " + e.getMessage());
+        } finally {
+            em.close();
+        }
+    }
+
+    public void delete(Long id) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            Curso curso = em.find(Curso.class, id);
+            if (curso != null) {
+                em.remove(curso);
+                System.out.println("Curso removido com sucesso!");
+            }
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            System.err.println("Erro ao remover o curso: " + e.getMessage());
+        } finally {
+            em.close();
+        }
+    }
+}
